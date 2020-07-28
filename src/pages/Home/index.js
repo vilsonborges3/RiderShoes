21b05cfe0d3b6e924b640b83e54c0286820fec45
Vilson Borges <vilsonborges98@gmail.com>
2020-07-28 14:34:50 -0300

    Home Styling

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,140 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
-function Home() {
-  return <div />;
+import { MdShoppingCart } from 'react-icons/md';
+
+export default function Home() {
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-everlast-kromus-masculino/26/AXB-1745-026/AXB-1745-026_zoom2.jpg?ts=1567596394&ims=326x"
+          alt="tenis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button>
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+           </div>
+           <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-everlast-kromus-masculino/26/AXB-1745-026/AXB-1745-026_zoom2.jpg?ts=1567596394&ims=326x"
+          alt="tenis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button>
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+           </div>
+           <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-everlast-kromus-masculino/26/AXB-1745-026/AXB-1745-026_zoom2.jpg?ts=1567596394&ims=326x"
+          alt="tenis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button>
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+           </div>
+           <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-everlast-kromus-masculino/26/AXB-1745-026/AXB-1745-026_zoom2.jpg?ts=1567596394&ims=326x"
+          alt="tenis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button>
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+           </div>
+           <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-everlast-kromus-masculino/26/AXB-1745-026/AXB-1745-026_zoom2.jpg?ts=1567596394&ims=326x"
+          alt="tenis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button>
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+           </div>
+           <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-everlast-kromus-masculino/26/AXB-1745-026/AXB-1745-026_zoom2.jpg?ts=1567596394&ims=326x"
+          alt="tenis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button>
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+           </div>
+           <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-everlast-kromus-masculino/26/AXB-1745-026/AXB-1745-026_zoom2.jpg?ts=1567596394&ims=326x"
+          alt="tenis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button>
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+           </div>
+           <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-everlast-kromus-masculino/26/AXB-1745-026/AXB-1745-026_zoom2.jpg?ts=1567596394&ims=326x"
+          alt="tenis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button>
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+           </div>
+           <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+    </ProductList>
+  );
 }
-
-export default Home;
