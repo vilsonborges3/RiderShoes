e29f7a03492dcba162023eaad5fb8cb4e7f24627
Vilson Borges <vilsonborges98@gmail.com>
2020-07-29 17:06:29 -0300

    setting saga with redux

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -29,16 +29,17 @@ class Home extends Component {
     this.setState({ products: data });
   }
 
-  handleAddProduct = product => {
-    const { addToCart } = this.props;
+  handleAddProduct = id => {
+    const { addToCartRequest } = this.props;
 
     /**into the dispatch we've got put us object it is the ACTION */
-    addToCart(product);
+    addToCartRequest(id);
 
   };
 
   render() {
     const { products } = this.state;
+    const { amount } = this.props;
 
     return (
       <ProductList>
@@ -51,9 +52,10 @@ class Home extends Component {
             <strong>{ product.title }</strong>
             <span>{ product.priceFormatted }</span>
 
-            <button type="button" onClick={() => this.handleAddProduct(product)} >
+            <button type="button" onClick={() => this.handleAddProduct(product.id)} >
               <div>
-                <MdAddShoppingCart size={16} color="#FFF" /> 3
+                <MdAddShoppingCart size={16} color="#FFF" />
+                  {amount[product.id] || 0}
               </div>
               <span>ADICIONAR AO CARRINHO</span>
             </button>
@@ -69,4 +71,13 @@ class Home extends Component {
 const mapDispatchToProps = dispatch =>
   bindActionCreators(CartActions, dispatch);
 
-export default connect(null, mapDispatchToProps)(Home);
+
+const mapStateToProps = state => ({
+  amount: state.cart.reduce((amount, product) => {
+    amount[product.id] = product.amount;
+
+    return amount;
+  }, {}),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
