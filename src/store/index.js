3b4f17bfe66a3f53790247e8fc3aed33fc243ed3
Vilson Borges <vilsonborges98@gmail.com>
2020-07-30 14:53:48 -0300

    checking the stock api

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,13 @@ import createSagaMiddlewere from 'redux-saga';
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
 
-const sagaMiddlewere = createSagaMiddlewere();
+const sagaMonitor = process.env.NODE_ENV === 'development'
+  ? console.tron.createSagaMonitor()
+  : null;
+
+const sagaMiddlewere = createSagaMiddlewere({
+  sagaMonitor
+});
 
 const enhancer =
   process.env.NODE_ENV === 'development'
