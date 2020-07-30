9431b4baa9301dee16673ad415ed7b019daaba5f
Vilson Borges <vilsonborges98@gmail.com>
2020-07-30 15:54:55 -0300

    finish aplication

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 
@@ -8,18 +8,19 @@ import './config/ReactotronConfig';
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
 
+import history from './services/history';
 import Routes from './routes';
 import store from './store';
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <Router history={history}>
         <Header />
         <Routes />
         <GlobalStyle />
         <ToastContainer autoClose={3000} />
-      </BrowserRouter>
+      </Router>
     </Provider>
   );
 }
